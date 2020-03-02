f857c0992991639a590a698d07174b617e5c9e9b
SHIDA Yuuma <m@crat.jp>
2020-03-02 14:53:23 +0900

    Fixed wrong feed contents

--- pages/api/feed.js
+++ pages/api/feed.js
@@ -4,7 +4,7 @@ import {getNews} from '../../lib/api';
 
 
 export default async function Feed(req, res) {
-    res.setHeader('Content-Type', 'text/xml; charset=utf-8');
+    res.setHeader('Content-Type', 'application/atom+xml; charset=utf-8');
     res.setHeader('Cache-Control', 'max-age=86400, public');
 
     const news = await getNews(100, undefined);
@@ -14,17 +14,16 @@ export default async function Feed(req, res) {
             '@xmlns': 'http://www.w3.org/2005/Atom',
             id: {'#text': `tag:${req.headers.host},2019:news/feed.xml`},
             title: {'@type': 'text', '#text': 'ちくわバー'},
-            link: {
+            link: [{
                 '@rel': 'alternate',
                 '@type': 'text/html',
                 '@hreflang': 'ja',
                 '@href': `https://${req.headers.host}`,
-            },
-            link: {
+            }, {
                 '@rel': 'self',
                 '@type': 'application/atom+xml',
                 '@href': `https://${req.headers.host}/news/feed.xml`,
-            },
+            }],
             updated: news.length > 0 ? {
                 '#text': news.reduce((x, y) => x.updatedAt > y.updatedAt ? x : y).updatedAt.toISOString(),
             } : undefined,
