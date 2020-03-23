11b464938fbc28676749e3c9e56fab6c32e42e6f
MacRat <m@crat.jp>
2020-03-23 19:45:00 +0900

    Decided logo image

--- components/FirstView/index.jsx
+++ components/FirstView/index.jsx
@@ -6,50 +6,19 @@ import RawHTML from '../RawHTML';
 import Instagram from '../Instagram';
 
 
-function LogoSelector({logo, setLogo}) {
-    return (
-        <div className="first-view--logo-selector">
-            <h1>見た目をいじるやつ</h1>
-            <button disabled={logo === 'dark'} onClick={() => setLogo('dark')}><img src="/logo/dark.png" alt="黒いロゴ" /></button>
-            <button disabled={logo === 'light'} onClick={() => setLogo('light')}><img src="/logo/light.png" alt="茶色いロゴ" /></button>
-            <style jsx>{`
-                div {
-                    position: fixed;
-                    bottom: 0;
-                    right: 0;
-                    padding: 16px 24px;
-                    background-color: rgba(255, 0, 0, .8);
-                    z-index: 1000;
-                }
-                h1 {
-                    font-size: 120%;
-                    color: white;
-                    margin: 0 0 4px;
-                }
-                button {
-                    display: block;
-                    width: 100%;
-                }
-                button:disabled {
-                    opacity: .5;
-                }
-                img {
-                    width: 120px;
-                    pointer-events: none;
-                }
-            `}</style>
-        </div>
-    );
-}
-
-
 function FirstView({image, topText}) {
-    const [logo, setLogo] = useState('dark');
-
     return (
         <header>
             <div>
-                <h1><img alt="ちくわバー" src={`/logo/${logo}.png`} /></h1>
+                <h1>
+                    <amp-img
+                        alt="ちくわバー"
+                        width="380"
+                        height="101"
+                        layout="intrinsic"
+                        srcset="/img/logo.png 1x, /img/logo@2x.png 2x"
+                        src="/img/logo.png" />
+                </h1>
                 <span>不慣れなおじさん一人でやってます</span>
             </div>
             <nav>
@@ -62,8 +31,6 @@ function FirstView({image, topText}) {
             <a href="https://www.instagram.com/tikuwa_world/" target="_blank">
                 <Instagram />
             </a>
-
-            <LogoSelector logo={logo} setLogo={setLogo} />
 
             <style jsx>{`
                 header {
@@ -92,10 +59,6 @@ function FirstView({image, topText}) {
                     margin: 0;
                     width: 100%;
                     color: white;
-                }
-                img {
-                    width: 380px;
-                    max-width: 100%;
                 }
                 span {
                     font-size: 24px;
